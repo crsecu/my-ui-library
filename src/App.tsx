cf908dfd871599f04740c9a2291747af1ca0be5b
CS <secu.cristina@yahoo.com>
2026-01-29 22:20:59 -0500

    add primary, secondary, and tertiary variants for the button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import { Button } from './components/Button/Button';
+import { FaPlus } from 'react-icons/fa';
 
 export const App = () => {
+  function handleClick() {
+    console.log('Button clicked');
+  }
+
+  function handleFocus() {
+    console.log('Button focused');
+  }
+
+  function handleBlur() {
+    console.log('Button blurred');
+  }
+
   return (
     <>
-      <p>My UI Library</p>
-      <Button>Sign up</Button>
+      <Button>Submit</Button>
+
+      <Button
+        onClick={handleClick}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        variant="secondary"
+      >
+        Submit
+      </Button>
+      <Button variant="tertiary">Submit</Button>
     </>
   );
 };
