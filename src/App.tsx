70ac8c3c842ea8b57c928d463a4f8e7acf79a0f4
CS <secu.cristina@yahoo.com>
2026-02-10 18:12:15 -0500

    feat: scaffold Button and define prop types

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { Button } from './components/Button/Button';
+
 export const App = () => {
-  return <p>My UI Library</p>;
+  return (
+    <>
+      <p>My UI Library</p>
+      <Button>Sign up</Button>
+    </>
+  );
 };
