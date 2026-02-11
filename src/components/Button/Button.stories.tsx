efda7d39d2d5b0aae02644ca315ed36896e2814a
CS <secu.cristina@yahoo.com>
2026-02-11 15:12:35 -0500

    write stories for secondary and tertiary button variants; add opacity CSS property for disabled buttons

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,9 +1,22 @@
 import type { Meta, StoryObj } from '@storybook/React';
 
+import { fn } from 'storybook/test';
+
 import { Button } from './Button';
+
+export const ActionsData = {
+  onClick: fn(),
+  onFocus: fn(),
+  onBlur: fn(),
+};
 
 const meta = {
   component: Button,
+  tags: ['autodocs'],
+  excludeStories: /.*Data$/,
+  args: {
+    ...ActionsData,
+  },
 } satisfies Meta<typeof Button>;
 
 export default meta;
@@ -12,10 +25,20 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {
-    children: 'Test',
-    variant: "secondary",
-    isLoading: false,
-    disabled: false,
-    tooltipPosition: "left"
+    children: 'Primary',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    children: 'Secondary',
+    variant: 'secondary',
+  },
+};
+
+export const Tertiary: Story = {
+  args: {
+    children: 'Tertiary',
+    variant: 'tertiary',
   },
 };
