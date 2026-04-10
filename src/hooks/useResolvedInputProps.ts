2cd6edd7302ec18a0823d9bf58336926e9f6042e
CS <secu.cristina@yahoo.com>
2026-04-10 17:20:00 -0400

    continue working on useResolvedInputProps hook

--- src/hooks/useResolvedInputProps.ts
+++ src/hooks/useResolvedInputProps.ts
@@ -9,6 +9,8 @@ disabled?: boolean
 
 */
 
+import { type FieldInputProps, type FieldMetaProps, useField } from 'formik';
+
 /* *** Formik controlled:
   Props:
  name: string
@@ -21,25 +23,63 @@ disabled?: boolean
  }
   setError(errorText?: string) => void
 */
-type ExternalControlled<T> = {
-  value: T;
-  onChange: (value: T) => void;
-  disabled?: boolean;
-};
 type FormikControlled = {
   name: string;
   disabled?: boolean;
 };
 
+type ExternalControlled<T> = {
+  value: T;
+  onChange: (value: T) => void;
+  disabled?: boolean;
+};
+
 type UseResolvedInputProps<T> = ExternalControlled<T> | FormikControlled;
 
-export const useResolvedInputProps = <T>(props: UseResolvedInputProps<T>) => {
-  if ('name' in props) {
-    //Input controlled by Formik
-    return { ...props };
-  } else {
-    //Input controlled Externally
-    return { inputProps: { ...props } };
+type ResolvedInputReturn<T> = {
+  mergedProps: ExternalControlled<T> | Pick<FieldInputProps<T>, 'value' | 'onChange'>;
+  setValue: (value: T, shouldValidate?: boolean) => void;
+  metaProps?: Pick<FieldMetaProps<T>, 'error' | 'touched' | 'initialValue'>;
+  setError?: (errorText?: string) => void;
+};
+
+//TO DO: memoize the return object
+export const useResolvedInputProps = <T>(
+  props: UseResolvedInputProps<T>,
+): ResolvedInputReturn<T> | null => {
+  let isFormik = false;
+  const name = ('name' in props && props.name) || '';
+
+  function setValue(value: T, shouldValidate?: boolean): void {
+    if ('onChange' in props) props.onChange(value);
   }
-  //return {};
+
+  try {
+    const fieldResult = useField(name);
+    isFormik = true;
+
+    console.log('fieldResult', fieldResult);
+
+    if ('name' in props && props.name && isFormik) {
+      const [field, meta, helpers] = fieldResult;
+      const { value, onChange } = field;
+      console.log('field', field, value);
+      const { error, touched, initialValue } = meta;
+
+      return {
+        mergedProps: { value, onChange, disabled: props.disabled },
+        setValue: helpers.setValue,
+        metaProps: { error, touched, initialValue },
+        setError: helpers.setError,
+      };
+    }
+  } catch (err) {
+    console.log('Hook called outside of Formik Context', err);
+  }
+
+  if ('value' in props && props.value) {
+    return { mergedProps: { ...props }, setValue };
+  }
+
+  return null;
 };
