f65245757ebd6ac7fd1e0cdc07334834d173b3ef
CS <secu.cristina@yahoo.com>
2026-05-10 11:38:19 -0400

    convert useOptionalUseField to generic, and add JSDoc

--- src/hooks/useOptionalUseField.ts
+++ src/hooks/useOptionalUseField.ts
@@ -1,8 +1,16 @@
 import { useField } from 'formik';
 
-export function useOptionalUseField(fieldName: string) {
+/**
+ * A wrapper around Formik's `useField` hook.
+ * Attempts to retrieve Formik field state and helpers for a given field name.
+ * Unlike the standard `useField`, this will not throw an error if the hook
+ * is called outside `<Formik>` context; instead, it returns `undefined`.
+ * @param fieldName - The name of the form field. This should match a key defined in Formik's `initialValues`.
+ * @returns The Field tuple [FieldInputProps, FieldMetaProps, FieldHelperProps] if within context, otherwise undefined.
+ */
+export function useOptionalUseField<T>(fieldName: string) {
   try {
-    return useField(fieldName);
+    return useField<T>(fieldName);
   } catch {
     return undefined;
   }
