506c2952c2a764926908811df4af9981d2e3eea4
CS <secu.cristina@yahoo.com>
2026-05-12 15:19:04 -0400

    save current progress of Input Component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,8 +1,33 @@
 import type { InputHTMLAttributes } from 'react';
+import {
+  type ExternalControlled,
+  type FormikControlled,
+  useResolvedInputProps,
+} from '../../hooks/useResolvedInputProps.tsx';
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  name: string;
+interface InputProps<T> extends Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'> {
+  labelText: string;
+  value?: T;
+  onChange: (value: T) => void;
 }
-export const Input = ({ ...props }: InputProps) => {
-  return <input {...props} />;
+export const Input = <T,>({
+  labelText,
+  value,
+  onChange,
+  name,
+  disabled,
+  ...props
+}: InputProps<T>) => {
+  const inputProps: ExternalControlled<T> | FormikControlled = !value
+    ? { value, onChange, disabled }
+    : { name, disabled };
+
+  const resolvedProps = useResolvedInputProps(inputProps);
+
+  return (
+    <label>
+      {labelText}
+      <input {...props} />
+    </label>
+  );
 };
