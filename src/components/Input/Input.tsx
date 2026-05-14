8bc1bd341593d9905e86eebcdf13d8defcb1269d
CS <secu.cristina@yahoo.com>
2026-05-14 15:26:20 -0400

    Extend generic parameter type to string to satisfy TS; extract value, onChange, disabled from props object to avoid type mismatches when spreading the props

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,20 +1,31 @@
 import type { InputHTMLAttributes } from 'react';
 import { useResolvedInputPropsRefactored } from '../../hooks/useResolvedInputPropsRefactored.ts';
 
-interface InputProps<T> extends Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'> {
+interface InputProps<T extends string> extends Omit<
+  InputHTMLAttributes<HTMLInputElement>,
+  'value' | 'onChange'
+> {
   labelText: string;
   value?: T;
-  onChange: (value: T) => void;
+  onChange?: (value: T) => void;
 }
 
-export const Input = <T,>({ labelText, ...props }: InputProps<T>) => {
-  const resolvedProps = useResolvedInputPropsRefactored(props);
+export const Input = <T extends string>({ labelText, ...props }: InputProps<T>) => {
+  const { value, onChange, disabled, ...restProps } = props;
+
+  const resolvedProps = useResolvedInputPropsRefactored({
+    name: props.name,
+    value,
+    onChange,
+    disabled,
+  });
+
   console.log('resolvedProps:', resolvedProps);
 
   return (
     <label>
       {labelText}
-      <input {...props} {...resolvedProps?.mergedProps} />
+      <input {...restProps} {...resolvedProps?.mergedProps} />
     </label>
   );
 };
