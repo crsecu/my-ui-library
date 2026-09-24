ca454d0ffa3fdb670fea31b6793a3ce464b37fb6
CS <secu.cristina@yahoo.com>
2026-09-24 15:52:03 -0400

    apply humanizeKey to format error field labels

--- src/components/ErrorLog/ErrorLog.tsx
+++ src/components/ErrorLog/ErrorLog.tsx
@@ -1,5 +1,6 @@
 import { BaseError } from '../../utils/BaseError.ts';
 import styles from '../ErrorLog/ErrorLog.module.css';
+import { humanizeKey } from '../../utils/humanizeKey.tsx';
 
 type OwnErrorKeys<T> = Exclude<keyof T, keyof Omit<Error, 'message'>>;
 
@@ -11,12 +12,15 @@ export const ErrorLog = ({ error }: ErrorLogProps) => {
   return (
     <>
       <ul className={styles.errorLogWrapper}>
-        {(Object.keys(error) as Array<OwnErrorKeys<typeof error>>).map((key) => (
-          <li key={key}>
-            <span className={styles.errorLabel}>{key}</span>
-            <span className={styles.errorValue}>{error[key]}</span>
-          </li>
-        ))}
+        {(Object.keys(error) as Array<OwnErrorKeys<typeof error>>).map((key) => {
+          const label = humanizeKey(key);
+          return (
+            <li key={key}>
+              <span className={styles.errorLabel}>{label}</span>
+              <span className={styles.errorValue}>{error[key]}</span>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
